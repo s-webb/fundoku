69106515f2e0c01ea04361d82e9f532148611eeb
Simon Webb <simon.webb@dunnhumby.com>
2016-06-23 09:21:22 +0100

    File parsing implementation using cats.Apply

--- src/main/scala/fundoku/Main.scala
+++ src/main/scala/fundoku/Main.scala
@@ -5,89 +5,83 @@ import java.nio.file.{Files, Paths}
 
 import scala.collection.JavaConverters._
 
-case class Cell(candidates: Seq[Int]) {
+import cats._
+import cats.syntax.cartesian._
 
-  override def toString: String =
-    if (candidates.length == 1) candidates(0).toString else "."
-}
+object Puzzle {
 
-object Cell {
+  type Cell = Set[Int]
+  type Puzzle = (String, Array[Cell])
 
-  def apply(c: Char): Cell = c match {
-    case '0' => new Cell(0 to 9)
-    case n => new Cell(Seq(n.toString.toInt))
+  def puzzle(name: String, cells: Array[Cell]): Puzzle = {
+    (name, cells)
   }
-}
 
-case class Unit(cells: Seq[Cell])
+  val emptyCell: Cell = (1 to 9).toSet
+  def completedCell(n: Int): Cell = Set(n)
 
-case class Puzzle(name: String, units: Seq[Unit]) {
-
-  override def toString: String = {
-    s"Puzzle: $name\n" +
-    "-------------\n" + 
-    "|" +
-    units(0).cells(0).toString +
-    units(0).cells(1).toString +
-    units(0).cells(2).toString +
-    "|" +
-    units(1).cells(0).toString +
-    units(1).cells(1).toString +
-    units(1).cells(2).toString +
-    "|" +
-    units(2).cells(0).toString +
-    units(2).cells(1).toString +
-    units(2).cells(2).toString +
-    "|\n" +
-    "|" +
-    units(0).cells(3).toString +
-    units(0).cells(4).toString +
-    units(0).cells(5).toString +
-    "|" +
-    units(1).cells(3).toString +
-    units(1).cells(4).toString +
-    units(1).cells(5).toString +
-    "|" +
-    units(2).cells(3).toString +
-    units(2).cells(4).toString +
-    units(2).cells(5).toString +
-    "|\n" +
-    "|" +
-    units(0).cells(6).toString +
-    units(0).cells(7).toString +
-    units(0).cells(8).toString +
-    "|" +
-    units(1).cells(6).toString +
-    units(1).cells(7).toString +
-    units(1).cells(8).toString +
-    "|" +
-    units(2).cells(6).toString +
-    units(2).cells(7).toString +
-    units(2).cells(8).toString +
-    "|\n" +
-    "-------------\n"
+  def charToCell(ch: Char): Option[Cell] = ch match {
+    case '0' => Some(emptyCell)
+    case n if n.isDigit => Some(completedCell(n.toString.toInt))
+    case _ => None
   }
-}
 
-object Main extends App {
+  type Parser[A] = Stream[Char] => Option[(A, Stream[Char])]
 
-  // Read each line of the file
-  val path = Paths.get("src/main/resources/p096_sudoku.txt")
-  val lines = Files.readAllLines(path, StandardCharsets.UTF_8).asScala
+  object Parsers {
 
-  val puzzles: Seq[Puzzle] = lines.grouped(10).map { puzzleLines =>
-    val name = puzzleLines(0)
-    val units: Seq[Unit] = puzzleLines.drop(1).grouped(3).flatMap { unitGroup =>
-      val unitCells: Seq[(Seq[Cell], Seq[Cell], Seq[Cell])] = unitGroup.map { l =>
-        val cells = l.map(Cell.apply)
-        (cells.slice(0, 3), cells.slice(3, 6), cells.slice(6, 9))
+    val grid: Parser[Array[Cell]] = in => {
+      val (l, r) = in.splitAt(90)
+      val cells = l.flatMap(charToCell)
+      if (cells.size == 81) Some(cells.toArray, r) else None
+    }
+
+    val name: Parser[String] = in => {
+      def step(cs: Stream[Char], acc: Option[String]): Option[(String, Stream[Char])] =
+        cs.headOption.flatMap { c =>
+          if (c == '\n') {
+            acc.map((_, cs.tail))
+          } else {
+            val stepAcc = acc.map(_ + c.toString) orElse Some(c.toString)
+            step(cs.tail, stepAcc)
+          }
+        }
+
+      step(in, None)
+    }
+
+    // implementation of map2 for parsers
+    def parse2[A, B, C](pa: Parser[A], pb: Parser[B], f: (A, B) => C): Parser[C] = s => {
+      for {
+        (a, s1) <- pa(s)
+        (b, s2) <- pb(s1)
+      } yield (f(a, b), s2)
+    }
+
+    val puzzle: Parser[Puzzle] = 
+      parse2(name, grid, Puzzle.puzzle)
+
+    // ... but I could get map2 from cats.Apply if I can implement ap and map for Parser[_]
+    implicit val parserApply: Apply[Parser] = new Apply[Parser] {
+      def ap[A, B](pf: Parser[A => B])(pa: Parser[A]): Parser[B] = s => {
+        for {
+          (f, s1) <- pf(s)
+          (a, s2) <- pa(s1)
+        } yield (f(a), s2)
       }
-      def cellsForUnit(f: ((Seq[Cell], Seq[Cell], Seq[Cell])) => Seq[Cell]): Seq[Cell] =
-        f(unitCells(0)) ++ f(unitCells(1)) ++ f(unitCells(2))
-      Seq(Unit(cellsForUnit(_._1)), Unit(cellsForUnit(_._2)), Unit(cellsForUnit(_._3)))
-    }.toSeq
-    Puzzle(name, units)
-  }.toSeq
 
-  println(puzzles(0))
+      def map[A, B](pa: Parser[A])(f: A => B): Parser[B] = s => {
+        pa(s).map { case (a, s1) =>
+          (f(a), s1)
+        }
+      }
+    }
+
+    val puzzle2: Parser[Puzzle] =
+      (name |@| grid) map { Puzzle.puzzle }
+
+    // I guess it could be handy to compose stuff like this if we had more parsers
+  }
+
+  // Implement toString for Puzzle...
 }
