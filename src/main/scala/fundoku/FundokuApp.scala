500921ba19f4b1e523327021ac51a83f7deeff1b
Simon Webb <simon.webb@dunnhumby.com>
2016-06-25 00:16:07 +0100

    Refactored SVG builders as pure functions, plus a bit of shapeless

--- src/main/scala/fundoku/FundokuApp.scala
+++ src/main/scala/fundoku/FundokuApp.scala
@@ -10,17 +10,18 @@ import scalatags.JsDom.svgTags._
 
 object FundokuApp extends JSApp {
 
-  val rowDims = Dims(9, 3, 30.0d, 0.10d, 0.20d)
   val colDims = Dims(9, 3, 30.0d, 0.10d, 0.20d)
+  val rowDims = Dims(9, 3, 30.0d, 0.10d, 0.20d)
+  val dims = (colDims, rowDims)
 
   val updateDelayMillis = 200
 
-  val cellActivity = Array.fill(81)(Activity.apply)
+  val cellActivity = Array.fill(dims.count)(Activity.apply)
 
   var tick = 0
   var updateIndex = 0
 
-  var currGridDom = mkGrid.render
+  var currGridDom = Builders.grid(dims)(intensity).render
 
   def main(): Unit = {
     document.body.appendChild(currGridDom)
@@ -31,7 +32,7 @@ object FundokuApp extends JSApp {
 
   def redraw(): Unit = {
     updateActivity()
-    val newGridDom = mkGrid.render
+    val newGridDom = Builders.grid(dims)(intensity).render
     document.body.replaceChild(newGridDom, currGridDom)
     currGridDom = newGridDom
     setTimeout(updateDelayMillis) {
@@ -39,7 +40,7 @@ object FundokuApp extends JSApp {
     }
   }
 
-  def toArrIndex(c: Int, r: Int): Int = (r * rowDims.count) + c
+  def toArrIndex(rowDims: Dims)(c: Int, r: Int): Int = (r * rowDims.count) + c
 
   // simulate an algorithm that accesses one cell 10 times in a tick,
   // then moves on to the next cell
@@ -64,48 +65,49 @@ object FundokuApp extends JSApp {
     updateIndex = wrappedAdd(updateIndex, 1)
   }
 
-  def mkGrid: Frag = {
-    import scalatags.JsDom.svgAttrs._
-    val cells =
-      for {
-        r <- (0 until rowDims.count)
-        c <- (0 until colDims.count)
-      } yield Seq(mkCell(c, r), mkCellText(c, r))
+  def intensity(col: Int, row: Int) = 1.0d - cellActivity(toArrIndex(rowDims)(col, row)).normalizedAggValue
+  // val colP = (col + row) / (colDims.count + rowDims.count).toDouble
+}
 
-    val bg = rect(
-      x := 0.0d,
-      y := 0.0d,
-      width := colDims.totalSize,
-      height := rowDims.totalSize,
-      fill := "black")
+object Builders {
 
-    svg(height := rowDims.totalSize, width := colDims.totalSize) (
-      bg +: cells.flatten
+  import Dims._
+  import Color._
+  import scalatags.JsDom.svgAttrs._
+
+  def grid(dims: GridDims)(intensity: (Int, Int) => Double): Frag = {
+    val cellFs: Seq[(GridDims, Int, Int) => Frag] = Seq(cell(intensity), cellText)
+    svg(height := dims.rows.totalSize, width := dims.cols.totalSize) (
+      background(dims) +: cellFs.flatMap(dims.mapCells)
     )
   }
 
-  def mkCell(col: Int, row: Int): Frag = {
-    import scalatags.JsDom.svgAttrs._
-    import Color._
-    val cellX = colDims.indexLoc(col)
-    val cellY = rowDims.indexLoc(row)
-    // val colP = (col + row) / (colDims.count + rowDims.count).toDouble
-    val intensity = 1.0d - cellActivity(toArrIndex(col, row)).normalizedAggValue
-    val fillCol = blend(white, red, intensity)
+  def background(dims: GridDims): Frag = {
+    rect(
+      x := 0.0d,
+      y := 0.0d,
+      width := dims.cols.totalSize,
+      height := dims.rows.totalSize,
+      fill := "black")
+  }
+
+  def cell(intensity: (Int, Int) => Double)(dims: GridDims, col: Int, row: Int): Frag = {
+    val cellX = dims.cols.indexLoc(col)
+    val cellY = dims.rows.indexLoc(row)
+    val fillCol = white.blend(red, intensity(col, row))
     rect(
       x := cellX,
       y := cellY,
-      width := colDims.size,
-      height := rowDims.size,
-      fill := fillCol.toSvg)
+      width := dims.cols.size,
+      height := dims.rows.size,
+      fill := Color.toSvg(fillCol))
   }
 
-  def mkCellText(col: Int, row: Int): Frag = {
-    import scalatags.JsDom.svgAttrs._
-    val textX = colDims.indexLoc(col) + (colDims.size * 0.5d)
-    val thinRow = ((row + 1) % rowDims.blockCount) != 0
-    val thinRowMod = if (thinRow) rowDims.thickPad * 0.5d else 0.0d
-    val textY = rowDims.indexLoc(row + 1) - (rowDims.size * 0.5d) + thinRowMod
+  def cellText(dims: GridDims, col: Int, row: Int): Frag = {
+    val textX = dims.cols.indexLoc(col) + (dims.cols.size * 0.5d)
+    val thinRow = ((row + 1) % dims.rows.blockCount) != 0
+    val thinRowMod = if (thinRow) dims.rows.thickPad * 0.5d else 0.0d
+    val textY = dims.rows.indexLoc(row + 1) - (dims.rows.size * 0.5d) + thinRowMod
     val cellText = if (thinRow) "1" else "0"
     text(
       x := textX, 
@@ -118,14 +120,43 @@ object FundokuApp extends JSApp {
 
 object Color {
 
+  import scala.language.implicitConversions
+
+  type Color = (Int, Int, Int)
+
+  val white: Color = (255, 255, 255)
+  val red: Color = (255, 0, 0)
+
   def blend(col1: Color, col2: Color, p: Double): Color = {
-    def blendEl(el: Color => Int): Int =
-      ((el(col1) * p) + (el(col2) * (1 - p))).toInt
-    Color(blendEl(_.r), blendEl(_.g), blendEl(_.b))
+    val mix: (Int, Int) => Int = (l, r) => ((l * p) + (r * (1 - p))).toInt
+    // has to be a better way of doing this
+    // (mix(col1._1, col2._1), mix(col1._2, col2._2), mix(col1._3, col2._3))
+
+    // something in shapeless maybe?
+    import shapeless._
+    import poly._
+    import syntax.std.tuple._
+    object mixP extends Poly1 {
+      implicit def caseInts = at[(Int, Int)](t => mix(t._1, t._2))
+    }
+    (col1 zip col2) map mixP
+    // ... it took 2 hours to come up with that, shapeless ftw
   }
 
-  val white = Color(255, 255, 255)
-  val red = Color(255, 0, 0)
+  def toSvg(color: Color): String = {
+    val (r, g, b) = color
+    s"rgb($r, $g, $b)"
+  }
+
+  implicit def toColorOps(col: Color): ColorOps = new ColorOps {
+    override def toSvg = Color.toSvg(col)
+    override def blend(other: Color, p: Double): Color = Color.blend(col, other, p)
+  }
+
+  trait ColorOps {
+    def toSvg: String
+    def blend(other: Color, p: Double): Color
+  }
 }
 
 object Activity {
@@ -145,8 +176,28 @@ case class Activity(ticks: Seq[Int]) {
   def normalizedAggValue: Double = normalize(aggregateValue)
 }
 
-case class Color(r: Int, g: Int, b: Int) {
-  def toSvg: String = s"rgb($r, $g, $b)"
+object Dims {
+
+  import scala.language.implicitConversions
+
+  type GridDims = (Dims, Dims)
+
+  implicit def gridDimsOps(dims: GridDims): GridDimsOps = new GridDimsOps {
+    override def cols = dims._1
+    override def rows = dims._2
+  }
+
+  trait GridDimsOps {
+    def cols: Dims
+    def rows: Dims
+    def count: Int = cols.count * rows.count
+    def mapCells[A](f: (GridDims, Int, Int) => A): Seq[A] = {
+      for { 
+        c <- (0 until cols.count)
+        r <- (0 until rows.count) 
+      } yield f((cols, rows), c, r)
+    }
+  }
 }
 
 case class Dims(
