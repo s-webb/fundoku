6f574974fe6a60d541187fa540ac3fac4931fa71
Frankie McGough <francis.mcgough@dunnumby.com>
2016-07-28 14:02:35 +0100

    It compiles! #shipit

--- src/main/scala/fundoku/ActivityPuzzle.scala
+++ src/main/scala/fundoku/ActivityPuzzle.scala
@@ -23,9 +23,54 @@ object ActivityPuzzle {
 
   type CellState[A] = State[Cells, A]
 
-  type ActivityPuzzleState[A] = State[(Cells, CellActivity), A]
+  type ActivityPuzzleState[A] = State[ActivityPuzzle, A]
+
+  type ActivityPuzzle = (Cells, CellActivity)
 
   val initialActivity: CellState[CellActivity]= State.inspect(cells => Seq.fill(cells.size)(0))
+
+  /**
+    * To animate this, you'd want eliminate once to give you back the new puzzle state and a per-cell activity value
+    */
+  def eliminateOnce(p: ActivityPuzzle): Boolean = {
+    val groups: Seq[Int => Seq[(Int, Int)]] = Seq(rowIndices, colIndices, unitIndices)
+    val eliminated =
+      for {
+        g <- groups
+        n <- 0 to 8
+      } yield {
+        eliminateCells(p, g(n))
+      }
+    eliminated.fold(false)(_ || _)
+  }
+
+  def rowIndices(r: Int): Seq[(Int, Int)] = (0 to 8).map((r, _))
+  def colIndices(c: Int): Seq[(Int, Int)] = (0 to 8).map((_, c))
+
+  def unitIndices(u: Int): Seq[(Int, Int)] = {
+    val firstRow = (u / 3) * 3
+    val lastRow = firstRow + 3
+    val firstCol = (u % 3) * 3
+    val lastCol = firstCol + 3
+    for {
+      r <- firstRow until lastRow
+      c <- firstCol until lastCol
+    } yield (r, c)
+  }
+
+  def eliminateCells(p: ActivityPuzzle, indices: Seq[(Int, Int)]): Boolean = {
+    val (complete, incomplete) = indices.partition(i =>
+      ActivityPuzzle.isCompleted(i._1, i._2).runA(p).value)
+    val completedDigits = complete.map(i => answer(i._1, i._2).runA(p).value)
+    val es = for {
+      index <- incomplete
+      digit <- completedDigits
+    } yield {
+      removeCandidate(index._1, index._2, digit).runA(p).value
+    }
+    es.fold(false)(_ || _)
+  }
+
 
   def totalActivity: ActivityPuzzleState[Int] = State.inspect{ case (cells, activity) => activity.sum }
 
@@ -60,34 +105,43 @@ object ActivityPuzzle {
   /**
    * Cost of removing a candidate is proportional to number of candidates in the cell
    */
-  def removeCandidate(r: Int, c: Int, candidate: Int): ActivityPuzzleState[Boolean] = State { case (cells, activity) =>
-    val index = rowAndColToIndex(r, c)
-    val currCandidates = cells(index)
+  def removeCandidate(r: Int, c: Int, candidate: Int): ActivityPuzzleState[Boolean] = {
+    val doTheThing: ActivityPuzzleState[(Boolean, Int)] = State { case (cells, activity) =>
+      val index = rowAndColToIndex(r, c)
+      val currCandidates = cells(index)
 
-    // Recursion with states???
-    @tailrec
-    def eliminate(candidates: Set[Int], newCandidates: Set[Int], eliminated: Boolean): (Set[Int], Boolean) = {
-      if (candidates.isEmpty) {
-        (newCandidates, eliminated)
-      } else {
-        val v = candidates.head
-        incActivity(r, c) // TODO - how to get recursion to play nicely with a state monad?
-        if (v != candidate) {
-          eliminate(candidates.tail, newCandidates + v, eliminated)
-        } else {
-          eliminate(candidates.tail, newCandidates, true)
-        }
-      }
+      val (newCandidates, eliminated) = eliminate(currCandidates, Set.empty, candidate, false)
+      (cells.updated(index, newCandidates) -> activity, (eliminated, currCandidates.size))
     }
 
-    val (newCandidates, eliminated) = eliminate(currCandidates, Set.empty, false)
-    (cells.updated(index, newCandidates) -> activity, eliminated)
+    doTheThing.flatMap{ case (eliminated, size) => incActivity(r, c, size).map(_ => eliminated) }
+//    for {
+//      (eliminated, size) <- doTheThing
+//      _ <- incActivity(r, c, size)
+//    } yield {
+//      eliminated
+//    }
   }
 
-  private def incActivity(r: Int, c: Int): ActivityPuzzleState[Unit] = State { case (cells, activity) =>
+  @tailrec
+  private def eliminate(candidates: Set[Int], newCandidates: Set[Int], candidate: Int, eliminated: Boolean): (Set[Int], Boolean) = {
+    if (candidates.isEmpty) {
+      (newCandidates, eliminated)
+    } else {
+      val v = candidates.head
+      if (v != candidate) {
+        eliminate(candidates.tail, newCandidates + v, candidate, eliminated)
+      } else {
+        eliminate(candidates.tail, newCandidates, candidate, eliminated = true)
+      }
+    }
+  }
+
+
+  private def incActivity(r: Int, c: Int, increment: Int = 1): ActivityPuzzleState[Unit] = State { case (cells, activity) =>
     val index = rowAndColToIndex(r, c)
     val curr = activity(index)
-    (cells -> activity.updated(index, curr + 1), ())
+    (cells -> activity.updated(index, curr + increment), ())
   }
 
   private def candidatesAt(r: Int, c: Int): ActivityPuzzleState[Set[Int]] = State.inspect {
--- src/test/scala/fundoku/ActivityPuzzleSpec.scala
+++ src/test/scala/fundoku/ActivityPuzzleSpec.scala
@@ -1,5 +1,6 @@
 package fundoku
 
+import fundoku.ActivityPuzzle.{CellActivity, Cells}
 import org.scalatest.{Matchers, WordSpecLike}
 
 class ActivityPuzzleSpec extends WordSpecLike with Matchers {
@@ -23,105 +24,67 @@ class ActivityPuzzleSpec extends WordSpecLike with Matchers {
   }
 
   "listCompleted" should {
-    "track activity" in {
-      val cells = Seq[Set[Int]] (
-        Set(1),
-        noneEliminated,
-        noneEliminated,
-        noneEliminated,
-        noneEliminated,
-        noneEliminated,
-        noneEliminated,
-        noneEliminated,
-        noneEliminated
-      )
-      val p = new ActivityPuzzle(cells)
-      p.activity(0, 0) should be (0)
-
-      val row = 0
-      val completedCells = (0 to 8).flatMap { col =>
-        if (p.isCompleted(row, col)) Some(col) else None
-      }
-      val completedDigits = completedCells.map { col =>
-        p.answer(row, col)
-      }
-      completedDigits should have size 1
-      p.activity(row, 0) should be (2)
-      (1 to 8).foreach { col =>
-        p.activity(row, col) should be (1)
-      }
-
-      for {
-        col <- 0 to 8
-        digit <- completedDigits
-      } {
-        if (!(completedCells contains col)) {
-          p.removeCandidate(row, col, digit)
-        }
-      }
-
-      (0 to 8).foreach { col =>
-        println(s"($row,$col): ${p.printCandidates(row, col)} (${p.activity(row, col)})")
-      }
-    }
+//    "track activity" in {
+//      import ActivityPuzzle._
+//      val cells = Seq[Set[Int]] (
+//        Set(1),
+//        noneEliminated,
+//        noneEliminated,
+//        noneEliminated,
+//        noneEliminated,
+//        noneEliminated,
+//        noneEliminated,
+//        noneEliminated,
+//        noneEliminated
+//      )
+//      val p: (Cells, CellActivity) = (cells, initialActivity.runEmptyA.value)
+//      ActivityPuzzle.activity(0, 0).run(p).value._2 should be (0)
+//
+//      val row = 0
+//      val completedCells = (0 to 8).flatMap { col =>
+//        if (isCompleted(row, col).run(p).value._2) Some(col) else None
+//      }
+//      val completedDigits = completedCells.map { col =>
+//        p.answer(row, col)
+//      }
+//      completedDigits should have size 1
+//      p.activity(row, 0) should be (2)
+//      (1 to 8).foreach { col =>
+//        p.activity(row, col) should be (1)
+//      }
+//
+//      for {
+//        col <- 0 to 8
+//        digit <- completedDigits
+//      } {
+//        if (!(completedCells contains col)) {
+//          p.removeCandidate(row, col, digit)
+//        }
+//      }
+//
+//      (0 to 8).foreach { col =>
+//        println(s"($row,$col): ${p.printCandidates(row, col)} (${p.activity(row, col)})")
+//      }
+//    }
   }
 
   "solve" should {
+    import ActivityPuzzle._
     "solve a puzzle" in {
-      val p = new ActivityPuzzle(puzzleCells)
+      val p: (Cells, CellActivity) = (puzzleCells, initialActivity.runA(puzzleCells).value)
       var eliminated = true
       var step = 0
-      println(s"Step: $step (activity: ${p.totalActivity})")
+      println(s"Step: $step (activity: ${totalActivity.runA(p).value})")
       println(p + "\n")
 
-      while (!p.isSolved && eliminated) {
+      while (!isSolved.runA(puzzleCells).value && eliminated) {
         step += 1
         eliminated = eliminateOnce(p)
-        println(s"Step: $step (activity: ${p.totalActivity})")
+        println(s"Step: $step (activity: ${totalActivity.runA(p).value})")
         println(p + "\n")
       }
-      println(s"solved? ${p.isSolved}")
+      println(s"solved? ${isSolved.runA(puzzleCells).value}")
     }
   }
 
-  /**
-   * To animate this, you'd want eliminate once to give you back the new puzzle state and a per-cell activity value
-   */
-  def eliminateOnce(p: ActivityPuzzle): Boolean = {
-    val groups: Seq[Int => Seq[(Int, Int)]] = Seq(rowIndices, colIndices, unitIndices)
-    val eliminated = 
-      for {
-        g <- groups
-        n <- 0 to 8
-      } yield {
-        eliminateCells(p, g(n))
-      }
-    eliminated.fold(false)(_ || _)
-  }
-
-  def rowIndices(r: Int): Seq[(Int, Int)] = (0 to 8).map((r, _))
-  def colIndices(c: Int): Seq[(Int, Int)] = (0 to 8).map((_, c))
-
-  def unitIndices(u: Int): Seq[(Int, Int)] = {
-    val firstRow = (u / 3) * 3
-    val lastRow = firstRow + 3
-    val firstCol = (u % 3) * 3
-    val lastCol = firstCol + 3
-    for {
-      r <- firstRow until lastRow
-      c <- firstCol until lastCol
-    } yield (r, c)
-  }
-
-  def eliminateCells(p: ActivityPuzzle, indices: Seq[(Int, Int)]): Boolean = {
-    val (complete, incomplete) = indices.partition(i => p.isCompleted(i._1, i._2))
-    val completedDigits = complete.map(i => p.answer(i._1, i._2))
-    val es = for {
-      index <- incomplete
-      digit <- completedDigits
-    } yield {
-      p.removeCandidate(index._1, index._2, digit)
-    }
-    es.fold(false)(_ || _)
-  }
 }
